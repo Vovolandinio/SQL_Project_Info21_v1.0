1c66fd6a51973b797510789542ffbd282e407edd
warbirdo <nanowatcher65@gmail.com>
2022-12-23 15:15:10 +0300

    The import procedure is done.

---
+++ src/part1/part1_import.sql
@@ -0,0 +1,13 @@
+-- Удаление данных из таблицы для последующего импорта.
+TRUNCATE tasks CASCADE;
+TRUNCATE peers CASCADE;
+
+
+CREATE or replace PROCEDURE pr_import(IN "table" text,IN path text,IN delimetr text)
+LANGUAGE plpgsql as $$
+BEGIN
+    EXECUTE format('COPY %s FROM %L WITH CSV DELIMITER %L HEADER;', $1, $2, $3);
+END; $$;
+
+
+call pr_import('tasks','/Users/warbirdo/Desktop/SQL_Project_Info21_v1.0/src/part1/import/tasks.csv',',');
