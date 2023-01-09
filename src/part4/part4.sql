b6e5d5e8f0e79fa0c0db8ec1c550979bf51d6e14
warbirdo <nanowatcher65@gmail.com>
2023-01-09 13:15:45 +0300

    4.1 is done. Need to check without loop

--- src/part4/part4.sql
+++ src/part4/part4.sql
@@ -1,8 +1,28 @@
-CREATE OR REPLACE PROCEDURE pr_remove_table(IN table_name varchar)
+-- 1) Создать хранимую процедуру, которая, не уничтожая базу данных, уничтожает все те таблицы текущей базы данных, имена которых начинаются с фразы 'TableName'.
+
+
+-- Создание таблицы для теста.
+CREATE TABLE returns_table (peer varchar, task varchar, xpamount integer);
+
+-- Удаление процедуры.
+DROP PROCEDURE IF EXISTS pr_remove_table(TableName varchar);
+
+-- Создание процедуры.
+CREATE OR REPLACE PROCEDURE pr_remove_table(IN TableName text)
 AS $$
-    BEGIN
-
-
-END;
+BEGIN
+FOR TableName  IN
+    SELECT  quote_ident(table_name)
+    FROM   information_schema.tables
+    WHERE  table_name LIKE TableName || '%'
+    AND    table_schema LIKE 'public'
+LOOP
+EXECUTE 'DROP TABLE ' || TableName;
+END LOOP;
+END
 $$ LANGUAGE plpgsql;
 
+-- Тестовая транзакция.
+BEGIN;
+CALL pr_remove_table('returns');
+END;
