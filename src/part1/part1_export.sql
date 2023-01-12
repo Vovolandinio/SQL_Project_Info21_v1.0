1a0b9002406bee642395423c86ab150f385fa2bc
warbirdo <nanowatcher65@gmail.com>
2023-01-12 16:18:44 +0300

    Find a bug. Rename from table to "table".

--- src/part1/part1_export.sql
+++ src/part1/part1_export.sql
@@ -1,4 +1,6 @@
-CREATE or replace PROCEDURE pr_export_to_csv_from_table(IN table text,IN path text,IN delimetr text)
+DROP PROCEDURE IF EXISTS pr_export_to_csv_from_table(table text, path text, delimetr text);
+
+CREATE or replace PROCEDURE pr_export_to_csv_from_table(IN "table" text,IN path text,IN delimetr text)
 LANGUAGE plpgsql as $$
 BEGIN
     EXECUTE format('COPY %s TO %L DELIMITER ''%s'' CSV HEADER;', $1, $2, $3);
