ccc74d5eed8806d22f7c263f7b52c3f99018edc8
daria <ohiamdaria@gmail.com>
2023-01-16 09:03:21 +0300

    13

--- src/part3/part3-2.sql
+++ src/part3/part3-2.sql
@@ -21,28 +21,85 @@ RETURNS SETOF returns_table AS $tab$
             INNER JOIN XP ON one.id = XP."Check"
             GROUP BY checks.peer, checks.task, xp.xpamount;
     END
-$tab$ LANGUAGE plpgsql
+$tab$ LANGUAGE plpgsql;
 
-drop  FUNCTION fnc_successful_checks
+drop  FUNCTION fnc_successful_checks;
 
 -- part3.9
 CREATE or replace FUNCTION fnc_successful_checks(task varchar)
-AS $tab$
+RETURNS SETOF returns_table AS $tab$
     BEGIN
         return query
             WITH one AS (SELECT *
                         FROM tasks
                         WHERE title LIKE concat('C', '%')),
                               -- concat('C', '%') AND title NOT LIKE concat('CPP', '%')),
-            last_task AS (SELECT MIN(title) AS title
+            last_task AS (SELECT MAX(title) AS title
                         FROM one),
-            date_of_successful_check AS (SELECT *
-                                         )
-            SELECT *
-            FROM fnc_successful_checks() AS successful_checks INNER JOIN last_task ON successful_checks.task = last_task.title
-    END
-$tab$ LANGUAGE plpgsql
+            date_of_successful_check AS (SELECT peer,
+                                                checks.task,
+                                                checks."Date"
+                        FROM checks
+                        INNER JOIN p2p ON checks.id = p2p."Check"
+                        INNER JOIN Verter ON checks.id = Verter."Check"
+                        WHERE p2p.state = 'Success' AND Verter.state = 'Success'
+                        GROUP BY checks.id)
 
+            SELECT peer AS Peer,
+                   date_of_successful_check."Date" AS Date
+            FROM date_of_successful_check INNER JOIN last_task ON date_of_successful_check.task = last_task.title
+    END
+$tab$ LANGUAGE plpgsql;
+
+drop  FUNCTION fnc_successful_checks;
+
+CREATE FUNCTION fnc_successful_checks_blocks(block1 varchar, block2 varchar)
+RETURNS TABLE(Started_block1 int, Started_block2 int, Started_both int, Started_not_both int)
+AS $$
+    BEGIN
+        WITH Started_block1 AS (SELECT peer
+        FROM Checks
+        WHERE task = concat(block1, '%')),
+        Started_block2 AS (SELECT peer
+        FROM Checks
+        WHERE task = concat(block2, '%')),
+        Started_both AS (SELECT peer
+        FROM Checks
+        WHERE task = concat(block2, '%') AND task = concat(block1, '%'))
+
+        SELECT
+        (SELECT COUNT(*)/9
+        FROM Started_block1) AS Started_block1,
+
+        (SELECT COUNT(*)/9
+        FROM Started_block2) AS Started_block2,
+
+        (SELECT COUNT(*)/9
+        FROM Started_both) AS Started_both,
+
+        (SELECT (9-COUNT(*))/9
+        FROM Started_both) AS Started_not_both;
+    END
+$$
+LANGUAGE plpgsql;
+
+CREATE FUNCTION fnc_successful_checks_birthday(block1 varchar)
+RETURNS TABLE(SuccessfulChecks integer, UnsuccessfulChecks integer)
+AS $$
+DECLARE
+    checks_count integer;
+BEGIN
+        SELECT MAX(Checks.id) INTO STRICT checks_count
+        FROM Checks;
+        SELECT (SELECT COUNT(*)/checks_count
+        FROM Peers INNER JOIN Checks ON Peers.birthday = Checks."Date"
+        WHERE Peers.Nickname = Checks.Peer) AS SuccessfulChecks,
+                (SELECT (checks_count - COUNT(*))/checks_count
+        FROM Peers INNER JOIN Checks ON Peers.birthday = Checks."Date"
+        WHERE Peers.Nickname = Checks.Peer) AS UnsuccessfulChecks;
+    END
+$$
+LANGUAGE plpgsql;
 
 
 
