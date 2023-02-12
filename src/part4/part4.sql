91cd5c01f6ae6a337a0d591268222e8c82dd9f03
warbirdo <nanowatcher65@gmail.com>
2023-02-12 14:50:15 +0300

    formatted code style in part 4.

--- src/part4/part4.sql
+++ src/part4/part4.sql
@@ -2,21 +2,27 @@
 -- уничтожает все те таблицы текущей базы данных, имена которых начинаются с фразы 'TableName'.
 
 -- Создание таблицы для теста.
-CREATE TABLE returns_table (peer varchar, task varchar, xpamount integer);
+CREATE TABLE returns_table
+(
+    peer     varchar,
+    task     varchar,
+    xpamount integer
+);
 
 DROP PROCEDURE IF EXISTS pr_remove_table(TableName varchar);
 
 CREATE OR REPLACE PROCEDURE pr_remove_table(IN TableName text)
-AS $$
+AS
+$$
 BEGIN
-FOR TableName  IN
-    SELECT  quote_ident(table_name)
-    FROM   information_schema.tables
-    WHERE  table_name LIKE TableName || '%'
-    AND    table_schema LIKE 'public'
-LOOP
-EXECUTE 'DROP TABLE ' || TableName;
-END LOOP;
+    FOR TableName IN
+        SELECT quote_ident(table_name)
+        FROM information_schema.tables
+        WHERE table_name LIKE TableName || '%'
+          AND table_schema LIKE 'public'
+        LOOP
+            EXECUTE 'DROP TABLE ' || TableName;
+        END LOOP;
 END
 $$ LANGUAGE plpgsql;
 
@@ -32,17 +38,23 @@ END;
 DROP PROCEDURE IF EXISTS pr_count_table(OUT n int);
 
 CREATE OR REPLACE PROCEDURE pr_count_table(OUT n int)
-AS $$
-    BEGIN
-    n = (SELECT count(*) FROM (SELECT routines.routine_name, parameters.data_type
-             FROM information_schema.routines
-             LEFT JOIN information_schema.parameters ON routines.specific_name=parameters.specific_name
-             WHERE routines.specific_schema='public' AND parameters.data_type IS NOT NULL ORDER BY routines.routine_name, parameters.ordinal_position) as foo);
+AS
+$$
+BEGIN
+    n = (SELECT count(*)
+         FROM (SELECT routines.routine_name, parameters.data_type
+               FROM information_schema.routines
+                        LEFT JOIN information_schema.parameters ON routines.specific_name = parameters.specific_name
+               WHERE routines.specific_schema = 'public'
+                 AND parameters.data_type IS NOT NULL
+               ORDER BY routines.routine_name, parameters.ordinal_position) as foo);
 END
-    $$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
 
-DO $$
-    DECLARE res integer;
+DO
+$$
+    DECLARE
+        res integer;
     BEGIN
         CALL pr_count_table(res);
         RAISE NOTICE 'Num %', res;
@@ -54,23 +66,24 @@ $$;
 
 DROP PROCEDURE IF EXISTS pr_delete_dml_triggers (IN ref refcursor, INOUT result int);
 
-CREATE OR REPLACE PROCEDURE pr_delete_dml_triggers (IN ref refcursor, INOUT result int)
-AS $$
+CREATE OR REPLACE PROCEDURE pr_delete_dml_triggers(IN ref refcursor, INOUT result int)
+AS
+$$
 BEGIN
-FOR ref IN
-    SELECT trigger_name || ' ON ' || event_object_table
-    FROM information_schema.triggers
-    WHERE trigger_schema = 'public'
-LOOP
-    EXECUTE 'DROP TRIGGER ' || ref;
-    result := result + 1;
-    END LOOP;
+    FOR ref IN
+        SELECT trigger_name || ' ON ' || event_object_table
+        FROM information_schema.triggers
+        WHERE trigger_schema = 'public'
+        LOOP
+            EXECUTE 'DROP TRIGGER ' || ref;
+            result := result + 1;
+        END LOOP;
 END
 $$ LANGUAGE plpgsql;
 
 -- Тестовая транзакция.
 BEGIN;
-CALL pr_delete_dml_triggers('cursor_name',0);
+CALL pr_delete_dml_triggers('cursor_name', 0);
 END;
 
 -- Проверка.
@@ -84,18 +97,19 @@ FROM information_schema.triggers;
 
 DROP PROCEDURE IF EXISTS pr_show_info (IN ref refcursor, IN name text);
 
-CREATE OR REPLACE PROCEDURE pr_show_info (IN ref refcursor, IN name text)
-AS $$
-    BEGIN
-OPEN ref FOR
-    SELECT routine_name,
-           routine_type,
-           routine_definition
-    FROM information_schema.routines
-    WHERE specific_schema='public'
-        AND routine_definition LIKE '%' || name || '%';
+CREATE OR REPLACE PROCEDURE pr_show_info(IN ref refcursor, IN name text)
+AS
+$$
+BEGIN
+    OPEN ref FOR
+        SELECT routine_name,
+               routine_type,
+               routine_definition
+        FROM information_schema.routines
+        WHERE specific_schema = 'public'
+          AND routine_definition LIKE '%' || name || '%';
 END
-    $$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
 
 
 -- Тестовая транзакция.
